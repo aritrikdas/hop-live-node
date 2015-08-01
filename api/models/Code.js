022475fa174e2e9128dc5937b080c1fe16265778
SanIsHereForYou <sdhar@tikkatech.com>
2015-08-02 01:46:21 +0530

    added model for codes

---
+++ api/models/Code.js
@@ -0,0 +1,14 @@
+/**
+* Code.js
+*
+* @description :: This model will hold all the Verification codes with mobile number
+*/
+
+
+module.exports = {
+
+  attributes: {
+    mobNo: {type: 'integer'},
+    verifictionCode: {type: 'string'}
+  }
+};
